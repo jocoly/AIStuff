be8c46e54e19b1da895d87bd1079af99e09a071d
Colin Lyell <josephlyell@gmail.com>
2025-11-11 02:09:44 -0500

    incorporated scryfall card lookup for mtg

--- bot.js
+++ bot.js
@@ -3,7 +3,11 @@ import {Client, Events, GatewayIntentBits} from 'discord.js';
 
 // models
 
-import {turbo} from "./commands/turbo.js";
+import {handleTurbo} from "./commands/turbo.js";
+
+// scryfall
+
+import {handleScryfall} from "./commands/scryfall.js"
 
 export const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
@@ -12,6 +16,7 @@ export var queue = [];
 const CONTAIN_BOT = process.env.CONTAIN_BOT === 'true';
 const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
 
+const SCRYFALL = process.env.SCRYFALL === 'true';
 const SD_TURBO = process.env.SD_TURBO === 'true';
 
 let isReply, refMsg, isCommand, isMention;
@@ -24,27 +29,21 @@ client.on(Events.MessageCreate, async msg => {
     let msgContent = msg.content;
     let msgAuthorId = msg.author.id;
 
-    if(msgAuthorId === botUserId && msg.content.substring(0,1) !== ('!')) return;
+    if(msgAuthorId === botUserId) return;
     if(CONTAIN_BOT && msg.channel.id !== DISCORD_CHANNEL_ID) return;
 
-    // all bot commands start with '!'
+    // bot commands start with '!'
     isCommand = Array.from(msgContent)[0] === '!' || Array.from(msgContent)[0];
 
+    if (SCRYFALL && await handleScryfall(msg)) return;
+    if (SD_TURBO && await handleTurbo(msg)) return;
     // if unable to find referenced message, returns an error and sets isReply to false
-    try{
-        refMsg = await msg.fetchReference();
-        isReply = true;
-    } catch(error){
-        isReply = false;
-    }
+    
 
-    if(msgContent.substring(0,5) === ("!test")) {
+    if(msgContent.startsWith("!test")) {
         await msg.reply("Hello world!");
     }
 
-    if(msgContent.substring(0,6) === ("!turbo") && SD_TURBO) {
-        await turbo(msg);
-    }
 })
 
 await client.login(process.env.DISCORD_TOKEN)
--- commands/turbo.js
+++ commands/turbo.js
@@ -4,15 +4,24 @@ import {processQueue} from "../tools/processQueue.js";
 
 const MAX_NUM_IMAGES = process.env.MAX_NUM_IMAGES;
 
-export async function turbo(msg) {
+export async function handleTurbo(msg) {
+    const content = msg.content.trim();
+    if (!content.startsWith("!turbo")) return false;
+
     const enqueueMessage = await msg.reply("Request added to the queue. There are " + queue.length + " requests ahead of you.");
     const numImages = await getNumImages(msg);
-    if(numImages > MAX_NUM_IMAGES){
+    if (numImages > MAX_NUM_IMAGES){
         await msg.reply("That's too many images. Try requesting " + MAX_NUM_IMAGES + " or fewer.");
+        return true;
     } else {
-        queue.push({msg, pipeline: 'SD_TURBO', enqueueMessageId: enqueueMessage.id});
+        queue.push({
+            msg,
+            pipeline: 'SD_TURBO',
+            enqueueMessageId: enqueueMessage.id
+        });
     }
     if (queue.length === 1) {
         await processQueue();
     }
+    return true;
 }
